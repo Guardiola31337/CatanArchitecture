3950e1007dcf2c1e1d93738767216c3fdc544b63
pgs <pgs@tid.es>
2015-07-21 15:34:36 +0200

    Initial commit

---
+++ AndroidFree/src/main/java/com/pguardiola/catanarchitecture/events/InMemoryEventsAdapter.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2015 Pablo Guardiola Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pguardiola.catanarchitecture.events;
+
+import com.pguardiola.catanarchitecture.hexagon.Callback;
+import com.pguardiola.catanarchitecture.hexagon.EventsPort;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class InMemoryEventsAdapter implements EventsPort {
+  private Map<Class, Set<Callback>> subscribers = new HashMap<Class, Set<Callback>>();
+  private List firedEvents = new ArrayList();
+
+  @Override public <T> void on(Class<T> type, Callback<T> command) {
+    if (!subscribers.containsKey(type)) subscribers.put(type, new HashSet<Callback>());
+    subscribers.get(type).add(command);
+  }
+
+  @Override @SuppressWarnings("unchecked") public void broadcast(Object event) {
+    firedEvents.add(event);
+    if (subscribers.containsKey(event.getClass())) {
+      for (Callback command : subscribers.get(event.getClass()))
+        command.call(event);
+    }
+  }
+
+  public boolean hasBeenFired(Class type) {
+    for (Object event : firedEvents)
+      if (event.getClass().equals(type)) return true;
+    return false;
+  }
+
+  @SuppressWarnings("unchecked") public <T> List<T> eventsOf(Class<T> type) {
+    List events = new ArrayList();
+    for (Object event : firedEvents)
+      if (event.getClass().equals(type)) events.add(event);
+    return events;
+  }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+CatanArchitecture
+====
+
+CatanArchitecture is a sample repository to illustrate Hexagonal architecture in Android.
+
+Developed By
+------------
+
+* Pablo Guardiola Sánchez - <guardiola31337@gmail.com>
+
+<a href="https://twitter.com/Guardiola31337">
+  <img alt="Follow me on Twitter" src="http://pguardiola.com/images/twitter-logo.png" />
+</a>
+<a href="https://es.linkedin.com/in/pabloguardiola">
+  <img alt="Add me to Linkedin" src="http://pguardiola.com/images/linkedin-logo.png" />
+</a>
+
+Contributors
+------------
+
+* [Pablo Guardiola Sánchez][1]
+
+License
+-------
+
+    Copyright 2015 Pablo Guardiola Sánchez
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+[1]: https://github.com/Guardiola31337
